beb5e03fea3f555b3f0f4ced69dcd4f8ae423cb1
Ashwini Suryawanshi <ashwinisuryawasnhi271@gmail.com>
2024-11-12 15:44:08 +0530

    CHANGES BY MASTER

---
+++ src/main/java/com/BillingSoftware/AdminController/UserController.java
@@ -0,0 +1,14 @@
+package com.BillingSoftware.AdminController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.*;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/home")
+	public String Home() {
+		return "index";
+	}
+
+}

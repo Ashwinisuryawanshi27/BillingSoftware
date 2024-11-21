bb3f9913eae9423478c3c71c20b77e1a8e85f207
kunal_chavan <chavankunal1929@gmail.com>
2024-11-21 12:40:55 +0530

    commit

--- src/main/java/com/BillingSoftware/AdminController/UserController.java
+++ src/main/java/com/BillingSoftware/AdminController/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.*;
 
 @Controller
-public class UserController {
+public class UserController {	
 
 	@GetMapping("/home")
 	public String Home() {
